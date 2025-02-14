add8e14539aa2c72781de9035d8d2623a527d287
Crispin Bevin <crispinjithin@gmail.com>
2025-02-14 11:49:11 +0530

    fin;

--- food-page/src/App.jsx
+++ food-page/src/App.jsx
@@ -6,30 +6,54 @@ import axios from 'axios'
 
 
 function App() {
-
-
   const [name, setName] = useState("")
   const [pass, setPass] = useState("")
 
-  const [data, setData] = useState('')
   
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
     console.log("Form Submitted",name,pass)
-    toast("Form Submitted ")
+
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(!regex.test(name)){
+      toast.error('Invalid email')
+    }
+
+    if(pass==""){
+      toast.error('Enter a password')
+    }
     
-    axios.post('https://foodpage.co.uk/v1/mobile/shop/admin/login',
-      {username: "learabia@gmail.com",
-      password: "e@123"}
+    const response = await axios.post('https://foodpage.co.uk/v1/mobile/shop/admin/login',
+      {
+        username: name,
+        password: pass,
+        deviceId: "",
+        type: "",
+        model: "",
+        appVersion: "",
+        token: "",
+        tokenUpdate: "No",
+      }
+    
+
+
     ).then(
-      response => console.log(response)
+      response => {
+        console.log(response)
+        if(response.status===200 || response.status===201){
+          toast.success('Logged In')
+        }
+      }
     )
   }
 
   return (
     <>
       <ToastContainer 
-        position='top-center'
+        // position='top-center'
+        // hideProgressBar={true}
+        // transition={slide}
       />
 
       <div className="login-section">
